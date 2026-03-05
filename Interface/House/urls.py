35eebf291681efcccb90af387f2c4247a067ae6e
Hadiaram Bashir <adm-hmbashir@darbeirut.com>
2026-03-05 12:35:07 +0200

    Removed logging

--- Interface/House/urls.py
+++ Interface/House/urls.py
@@ -44,8 +44,7 @@ urlpatterns = [
     path('index/FillWallGaps/', views.FillWallGaps),
     path('index/SnapRooms/', views.SnapRooms),
     path('index/FillLivingRoom/', views.FillLivingRoom),
-    path('index/Log_Boundaries/', views.Log_Boundaries),
-    path('index/Log_Graph/', views.Log_Graph),
+
 
     path('home', views.home),
 
